eda0b5fcf545900c989bb12b52d7d49a2fd3848a
Andrew <andslobby@gmail.com>
2021-05-18 16:07:10 +0300

    fix: find 9 compilation error

--- db.js
+++ db.js
@@ -3,8 +3,9 @@ require('dotenv').config();
 
                                 //database username   password
 const sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: 'localhost',
-    dialect: 'postgres'
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    operatorsAliases: false
 })
 
 sequelize.authenticate().then(
