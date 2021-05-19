5d8526977eb06dc773b2117036c650d0095024ea
Andrew <andslobby@gmail.com>
2021-05-19 10:04:13 +0300

    fix: Game.findAll method error

--- controllers/gamecontroller.js
+++ controllers/gamecontroller.js
@@ -2,11 +2,11 @@ var router = require('express').Router();
 var Game = require('../models/game');
 
 router.get('/all', (req, res) => {
-    Game.findAll({ where: { owner_id: req.user.id } })
+    Game.findAll({ where: { owner_id: req.body.user.id } })
         .then(
             function findSuccess(data) {
                 res.status(200).json({
-                    games: games,
+                    games: data,
                     message: "Data fetched."
                 })
             },
@@ -39,7 +39,7 @@ router.get('/:id', (req, res) => {
 router.post('/create', (req, res) => {
     Game.create({
         title: req.body.game.title,
-        owner_id: req.body.game.user.id,
+        owner_id: req.body.user.id,
         studio: req.body.game.studio,
         esrb_rating: req.body.game.esrb_rating,
         user_rating: req.body.game.user_rating,
